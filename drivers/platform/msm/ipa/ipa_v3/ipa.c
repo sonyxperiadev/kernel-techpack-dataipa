bb9e238e2a307dd5cea7b8dc1c3848ee8d783222
Pavel Dubrova <pashadubrova@gmail.com>
2025-01-27 21:29:33 +0200

    Revert "msm: ipa: load IPA FW after smmu CB are probed"
    This reverts commit https://github.com/sonyxperiadev/kernel/commit/d3e8fba561bd73fbad39fa5a0c9856eccb4a5d34
    
    Do not attempt to load the firmware immediately after
    probing the SMMU. We are building the IPA driver as
    built-in, which means it will try to load the firmware
    immediately, even in recovery. To prevent this and ensure
    the firmware is only loaded via a userspace trigger, as
    it was before, revert this change.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -6923,9 +6923,6 @@ static void ipa3_load_ipa_fw(struct work_struct *work)
 		return;
 	}
 	IPA_ACTIVE_CLIENTS_DEC_SIMPLE();
-	mutex_lock(&ipa3_ctx->fw_load_data.lock);
-	ipa3_ctx->fw_load_data.state = IPA_FW_LOAD_STATE_LOADED;
-	mutex_unlock(&ipa3_ctx->fw_load_data.lock);
 	pr_info("IPA FW loaded successfully\n");
 
 	result = ipa3_post_init(&ipa3_res, ipa3_ctx->cdev.dev);
@@ -6955,58 +6952,6 @@ static void ipa3_load_ipa_fw(struct work_struct *work)
 		}
 		IPADBG("IPA uC PIL loading succeeded\n");
 	}
-}
-
-static void ipa_fw_load_sm_handle_event(enum ipa_fw_load_event ev)
-{
-	mutex_lock(&ipa3_ctx->fw_load_data.lock);
-
-	IPADBG("state=%d event=%d\n", ipa3_ctx->fw_load_data.state, ev);
-
-	if (ev == IPA_FW_LOAD_EVNT_FWFILE_READY) {
-		if (ipa3_ctx->fw_load_data.state == IPA_FW_LOAD_STATE_INIT) {
-			ipa3_ctx->fw_load_data.state =
-				IPA_FW_LOAD_STATE_FWFILE_READY;
-			goto out;
-		}
-		if (ipa3_ctx->fw_load_data.state ==
-			IPA_FW_LOAD_STATE_SMMU_DONE) {
-			ipa3_ctx->fw_load_data.state =
-				IPA_FW_LOAD_STATE_LOAD_READY;
-			goto sched_fw_load;
-		}
-		IPAERR("ignore multiple requests to load FW\n");
-		goto out;
-	}
-	if (ev == IPA_FW_LOAD_EVNT_SMMU_DONE) {
-		if (ipa3_ctx->fw_load_data.state == IPA_FW_LOAD_STATE_INIT) {
-			ipa3_ctx->fw_load_data.state =
-				IPA_FW_LOAD_STATE_SMMU_DONE;
-			goto sched_fw_load;
-		}
-		if (ipa3_ctx->fw_load_data.state ==
-			IPA_FW_LOAD_STATE_FWFILE_READY) {
-			ipa3_ctx->fw_load_data.state =
-				IPA_FW_LOAD_STATE_LOAD_READY;
-			goto sched_fw_load;
-		}
-		IPAERR("ignore multiple smmu done events\n");
-		goto out;
-	}
-	IPAERR("invalid event ev=%d\n", ev);
-	mutex_unlock(&ipa3_ctx->fw_load_data.lock);
-	ipa_assert();
-	return;
-
-out:
-	mutex_unlock(&ipa3_ctx->fw_load_data.lock);
-	return;
-
-sched_fw_load:
-	IPADBG("Scheduled a work to load IPA FW\n");
-	mutex_unlock(&ipa3_ctx->fw_load_data.lock);
-	queue_work(ipa3_ctx->transport_power_mgmt_wq,
-		&ipa3_fw_loading_work);
 }
 
 static ssize_t ipa3_write(struct file *file, const char __user *buf,
@@ -7088,8 +7033,17 @@ static ssize_t ipa3_write(struct file *file, const char __user *buf,
 			ipa3_ctx->ipa_config_is_mhi ? "" : "non ");
 	}
 
-	ipa_fw_load_sm_handle_event(IPA_FW_LOAD_EVNT_FWFILE_READY);
+	/* Prevent multiple calls from trying to load the FW again. */
+	if (ipa3_ctx->fw_loaded) {
+		IPAERR("not load FW again\n");
+		return count;
+	}
 
+	/* Schedule WQ to load ipa-fws */
+	ipa3_ctx->fw_loaded = true;
+	queue_work(ipa3_ctx->transport_power_mgmt_wq,
+		&ipa3_fw_loading_work);
+	IPADBG("Scheduled a work to load IPA FW\n");
 	return count;
 }
 
@@ -7297,9 +7251,6 @@ static int ipa3_pre_init(const struct ipa3_plat_drv_res *resource_p,
 		result = -ENOMEM;
 		goto fail_mem_ctx;
 	}
-
-	ipa3_ctx->fw_load_data.state = IPA_FW_LOAD_STATE_INIT;
-	mutex_init(&ipa3_ctx->fw_load_data.lock);
 
 	ipa3_ctx->logbuf = ipc_log_context_create(IPA_IPC_LOG_PAGES, "ipa", 0);
 	if (ipa3_ctx->logbuf == NULL)
@@ -9363,28 +9314,6 @@ static int ipa3_smp2p_probe(struct device *dev)
 	return 0;
 }
 
-static void ipa_smmu_update_fw_loader(void)
-{
-	int i;
-
-	if (smmu_info.arm_smmu) {
-		IPADBG("smmu is enabled\n");
-		for (i = 0; i < IPA_SMMU_CB_MAX; i++) {
-			if (!smmu_info.present[i]) {
-				IPADBG("CB %d not probed yet\n", i);
-				break;
-			}
-		}
-		if (i == IPA_SMMU_CB_MAX ||
-			ipa3_ctx->num_smmu_cb_probed == ipa3_ctx->max_num_smmu_cb) {
-			IPADBG("All %d CBs probed\n", IPA_SMMU_CB_MAX);
-			ipa_fw_load_sm_handle_event(IPA_FW_LOAD_EVNT_SMMU_DONE);
-		}
-	} else {
-		IPADBG("smmu is disabled\n");
-	}
-}
-
 void ipa3_plat_drv_shutdown(struct platform_device *pdev_p)
 {
 	int res;
@@ -9461,7 +9390,6 @@ int ipa3_plat_drv_probe(struct platform_device *pdev_p)
 		cb->dev = dev;
 		smmu_info.present[IPA_SMMU_CB_AP] = true;
 		ipa3_ctx->num_smmu_cb_probed++;
-		ipa_smmu_update_fw_loader();
 
 		return 0;
 	}
@@ -9475,7 +9403,6 @@ int ipa3_plat_drv_probe(struct platform_device *pdev_p)
 		cb->dev = dev;
 		smmu_info.present[IPA_SMMU_CB_WLAN] = true;
 		ipa3_ctx->num_smmu_cb_probed++;
-		ipa_smmu_update_fw_loader();
 
 		return 0;
 	}
@@ -9489,7 +9416,6 @@ int ipa3_plat_drv_probe(struct platform_device *pdev_p)
 		cb->dev = dev;
 		smmu_info.present[IPA_SMMU_CB_UC] = true;
 		ipa3_ctx->num_smmu_cb_probed++;
-		ipa_smmu_update_fw_loader();
 
 		return 0;
 	}
@@ -9503,7 +9429,6 @@ int ipa3_plat_drv_probe(struct platform_device *pdev_p)
 		cb->dev = dev;
 		smmu_info.present[IPA_SMMU_CB_11AD] = true;
 		ipa3_ctx->num_smmu_cb_probed++;
-		ipa_smmu_update_fw_loader();
 
 		return 0;
 	}
@@ -9555,7 +9480,6 @@ int ipa3_plat_drv_probe(struct platform_device *pdev_p)
 			IPAERR("ipa3_init failed\n");
 			return result;
 		}
-		ipa_fw_load_sm_handle_event(IPA_FW_LOAD_EVNT_SMMU_DONE);
 		goto skip_repeat_pre_init;
 	}
 
